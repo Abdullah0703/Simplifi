6871acdb972b37f0ecc3642402e1121ae484357b
Abdullah0703 <abdullahimtiaz371@gmail.com>
2025-07-15 20:26:30 +0500

    Initial Commit to Simplifi Assessment

---
+++ src/main/java/com/simplifi/simplifi/SimplifiApplication.java
@@ -0,0 +1,13 @@
+package com.simplifi.simplifi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimplifiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimplifiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/simplifi/simplifi/SimplifiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simplifi.simplifi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimplifiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
